71352840dd071c2df1219230b99c47e794910c5f
Icido <rcjones9@hotmail.com>
2019-05-09 21:07:58 +0100

    A* Implemented!

--- Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -7,25 +7,79 @@ public class EnemyMovement : MonoBehaviour {
     Transform player;
     PlayerHealth playerHealth;
     EnemyHealth enemyHealth;
+
     public float speed = 1f;
+
+    AStar aStarPathfinding;
+
+    float distanceToPlayer;
+
+    float distanceBetween;
+    float distanceToNextNode;
+    float nextNodeNear = 1f;
+    int nodeNumber = 0;
+    int maxNodeNum;
+
+    List<Vector3> path = new List<Vector3>();
+    Vector3 nextNode = new Vector3();
+
+    private float minDistanceToPlayer = 1f;
+
+    private float distanceUntilChase = 10f;
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         playerHealth = player.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
-
+        aStarPathfinding = GetComponent<AStar>();
     }
 
 
     void Update()
     {
         //TODO: PATHFINDING
+        distanceBetween = Vector3.Distance(transform.position, player.position);
+
+        if(distanceBetween <= distanceUntilChase)
+        {
+            aStarPathfinding.pathFind(transform.position, player.position);
+        }
+
+        if(aStarPathfinding.finishedPath)
+        {
+            path = aStarPathfinding.storedPath;
+            nodeNumber = 0;
+            maxNodeNum = path.Count;
+            if (path.Count > 0)
+            {
+                nextNode = path[nodeNumber];
+                nextNode.y = 1f;
+            }
+        }
+
+        distanceToNextNode = Vector3.Distance(transform.position, nextNode);
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToNextNode < nextNodeNear)
+        {
+            nodeNumber++;
+            if (nodeNumber < maxNodeNum)
+            {
+                nextNode = path[nodeNumber];
+                nextNode.y = 1f;
+            }
+            
+            if(distanceToPlayer < minDistanceToPlayer)
+            {
+                nextNode = player.position;
+            }
+        }
 
         if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         {
             //Still alive and move
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+            transform.position = Vector3.MoveTowards(transform.position, nextNode, 0.05f);
         }
         else
         {
---
+++ Assets/Scripts/Enemy Scripts/Pathfinding/Scripts/GridNodes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNodes {
+
+
+    public int gridPosX;
+    public int gridPosY;
+
+    public bool isObstructed;
+    public Vector3 pos;
+
+    public GridNodes Parent;
+
+    public int distFromStart; //gCost
+    public int distFromEnd; //hCost or Manhatten Distance
+
+
+    public int FCost;
+
+    void setFCost()
+    {
+        FCost = distFromStart + distFromEnd;
+    }
+
+    public GridNodes(bool cIsWall, Vector3 cPos, int cGridX, int cGridY)
+    {
+        isObstructed = cIsWall;
+        pos = cPos;
+        gridPosX = cGridX;
+        gridPosY = cGridY;
+    }
+}
+
+
